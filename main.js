5c0f58f38694cf3a2f17299003e7966b6757bd7c
Demo User <demouser@MacBook-Pro.local>
2015-07-20 18:06:40 -0700

    Georgia is next to me

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function () {
 
+  $ curl -u "graceobrien" https://gracebrien.github.io 
+
   $('#button').click( function () {
   console.log ( $('#questions').val());
   return false;
